401ffb0aa2b28278dafedff07cc021b52fd4ffae
Koo <koosco136@gmail.com>
2025-12-31 05:30:44 +0900

    fix: init bracket error

--- src/main/kotlin/storage/DiskManager.kt
+++ src/main/kotlin/storage/DiskManager.kt
@@ -21,44 +21,44 @@ class DiskManager(
     init {
         Files.createDirectories(dbPath.parent)
         dbFile = DbFile(dbPath)
+    }
 
-        /**
-         * pageId에 해당하는 페이지를 buffer에 읽어들임
-         * buffer에 정확히 하나의 페이지를 채움
-         */
-        fun readPage(pageId: PageId, buffer: ByteBuffer) {
-            require(buffer.capacity() == Page.PAGE_SIZE)
+    /**
+     * pageId에 해당하는 페이지를 buffer에 읽어들임
+     * buffer에 정확히 하나의 페이지를 채움
+     */
+    fun readPage(pageId: PageId, buffer: ByteBuffer) {
+        require(buffer.capacity() == Page.PAGE_SIZE)
 
-            buffer.clear()
-            val offset = pageId * Page.PAGE_SIZE
-            dbFile.channel.read(buffer, offset)
-            buffer.flip()
-        }
+        buffer.clear()
+        val offset = pageId * Page.PAGE_SIZE
+        dbFile.channel.read(buffer, offset)
+        buffer.flip()
+    }
 
-        /**
-         * buffer의 내용을 pageId에 해당하는 페이지에 기록
-         */
-        fun writePage(pageId: PageId, buffer: ByteBuffer) {
-            require(buffer.capacity() == Page.PAGE_SIZE)
+    /**
+     * buffer의 내용을 pageId에 해당하는 페이지에 기록
+     */
+    fun writePage(pageId: PageId, buffer: ByteBuffer) {
+        require(buffer.capacity() == Page.PAGE_SIZE)
 
-            buffer.rewind()
-            val offset = pageId * Page.PAGE_SIZE
-            dbFile.channel.write(buffer, offset)
-        }
+        buffer.rewind()
+        val offset = pageId * Page.PAGE_SIZE
+        dbFile.channel.write(buffer, offset)
+    }
 
-        /**
-         * 새 페이지 할당 (append to file)
-         */
-        fun allocatePage(): PageId {
-            val newPageId = dbFile.size() / Page.PAGE_SIZE
-            val zero = ByteBuffer.allocate(Page.PAGE_SIZE)
-            dbFile.channel.write(zero, newPageId * Page.PAGE_SIZE)
+    /**
+     * 새 페이지 할당 (append to file)
+     */
+    fun allocatePage(): PageId {
+        val newPageId = dbFile.size() / Page.PAGE_SIZE
+        val zero = ByteBuffer.allocate(Page.PAGE_SIZE)
+        dbFile.channel.write(zero, newPageId * Page.PAGE_SIZE)
 
-            return newPageId
-        }
+        return newPageId
+    }
 
-        fun close() {
-            dbFile.close()
-        }
+    fun close() {
+        dbFile.close()
     }
 }
